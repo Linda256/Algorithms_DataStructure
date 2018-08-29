43b8e7cf38949afe71596a39e75a8f596d0a0baf
Linda Wang <lindawang256@gmail.com>
2018-08-29 12:30:02 -0700

    Insertion Sort

---
+++ sort/InsertionSort.js
@@ -0,0 +1,31 @@
+const InsertionSort = function(arr){
+  //loop through arr start with index 1;
+  //   curr index = i;
+  //   prev index = i-1;
+  //    while prev !== null and current item < previous item
+  //       prev=prev -1
+  //    if curr item < prev item
+  //       swap curr and prev;
+  //    return arr;
+  for (let i=1; i<arr.length; i++){
+    let curr =i;
+    let prev = i-1;
+    while ( arr[curr] < arr[prev]){
+      swap(curr, prev, arr)
+      curr=prev;
+      if (prev===0) break;
+      prev--;
+    }
+  }
+  return arr;
+}
+
+function swap(i,j,arr) {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+let arr = [100,7,8,5,13,4,6];
+InsertionSort(arr);
+console.log(arr);
