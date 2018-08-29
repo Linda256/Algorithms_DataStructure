31cb50be22466192e6b66d79e39e2f58c0efcf0e
Linda Wang <lindawang256@gmail.com>
2018-08-29 11:29:02 -0700

    Add Time Complexity

--- sort/SelectionSort.js
+++ sort/SelectionSort.js
@@ -1,4 +1,4 @@
-const selectSort = function(arr){
+const SelectionSort = function(arr){
   let currI= 0;
   for (let i=1;i<arr.length; i++){
     let minI = i;
@@ -22,5 +22,9 @@ function swap(i,j,arr) {
 }
 
 let arr = [7,8,5,13,4,6];
-selectSort(arr);
+SelectionSort(arr);
 console.log(arr);
+
+//Time complexity:
+// Worst: O(n*n) Best: O(n*n) Average: O(n*n)
+//Stable: No
